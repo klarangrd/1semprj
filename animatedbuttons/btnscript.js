65303e7e8172c715a037e50c8c9510d86d464d55
VINCENT LORENZEN <vincent.lorenzen@gmail.com>
2023-11-27 10:21:12 +0100

    ingen kommentar

--- animatedbuttons/btnscript.js
+++ animatedbuttons/btnscript.js
@@ -1,3 +1,4 @@
+//Funktion der gennem querySelector scroller ned til et givent element, samt er gjort langsomere gennem duration
 function smoothScroll(target) {
   var targetElement = document.querySelector(target);
   window.scrollTo({
