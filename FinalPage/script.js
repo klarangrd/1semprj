83293d94e6cd7726ff82440549630cf1f656fd83
HjalteHH <hjal0550@skivecollege.dk>
2023-12-11 10:58:11 +0100

    port 10000

--- FinalPage/script.js
+++ FinalPage/script.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = null;
+const port = 10000;
 const { Client } = require("pg");
 const pg = require("pg");
 const cors = require("cors");
